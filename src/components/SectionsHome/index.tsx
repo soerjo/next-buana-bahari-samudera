b0c89843fa2d10b5061829815a0b2f9e58b1f220
suryo hastomo <ryohastomo@gmail.com>
2023-09-19 17:58:25 +0700

    [fixing] - fixing responsive view section

--- src/components/SectionsHome/index.tsx
+++ src/components/SectionsHome/index.tsx
@@ -4,18 +4,19 @@ import SectionComp from "../SectionComp";
 const SectionHome = () => {
     return (
         <SectionComp id="home" classname="bg-[#00001F]">
-            <div className="container m-auto w-full h-full flex flex-col justify-center items-center">
-                <h1 className="text-white text-center text-6xl md:text-8xl font-bold mb-4">
-                    SERVICE CARGO WITH PRIDE AND SAFETY
-                </h1>
-                <p className="text-white text-center py-8">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ex
-                    reprehenderit quos sed modi! Id harum, incidunt reprehenderit natus
-                    minus provident accusamus error minima consequuntur blanditiis
-                    libero necessitatibus sapiente fugit.
-                </p>
-
-                <button className="bg-white rounded-full py-4 px-12 font-bold text-lg">Booking Now</button>
+            <div className="h-screen flex justify-center items-center">
+                <div className="container px-8 flex flex-col justify-center items-center">
+                    <h1 className="text-white text-center text-6xl md:text-8xl font-bold mb-4">
+                        SERVICE CARGO WITH PRIDE AND SAFETY
+                    </h1>
+                    <p className="text-white text-center py-8">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ex
+                        reprehenderit quos sed modi! Id harum, incidunt reprehenderit natus
+                        minus provident accusamus error minima consequuntur blanditiis
+                        libero necessitatibus sapiente fugit.
+                    </p>
+                    <button className="bg-white rounded-full py-4 px-12 font-bold text-lg mx-auto">Booking Now</button>
+                </div>
             </div>
         </SectionComp>
     );
