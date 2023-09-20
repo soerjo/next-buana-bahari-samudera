51a2114725ea0dbb0b3c93cb8d3ca9e12018ae35
suryo hastomo <ryohastomo@gmail.com>
2023-09-21 00:42:59 +0700

    [update] - update at section information

--- src/components/SectionInformation/index.tsx
+++ src/components/SectionInformation/index.tsx
@@ -1,31 +1,38 @@
+'use client'
+
 import React from 'react'
 import SectionComp from '../SectionComp'
 import listdata from './listdata.json'
+import Image from 'next/image'
 
 const SectionInformation = () => {
     return (
-        <SectionComp id="information" classname="bg-blue-950 ">
-            <div className="w-full h-full flex justify-center items-center flex-col py-8 gap-8 container p-8 mx-auto">
+        <SectionComp id="information" classname="bg-white">
+            <div className="relative w-full h-full flex justify-center items-center flex-col py-8 gap-8 container p-8 mx-auto">
                 <div className="max-w-[653px] mx-auto flex flex-col justify-center items-center pt-14 md:pt-12">
-                    <h1 className="text-white text-center text-5xl font-bold mb-2">
+                    <h1 className="text-[#2b1656] text-center text-5xl font-bold mb-2">
                         {listdata.title}
                     </h1>
-                    <p className="text-white text-center py-4 font-light">
+                    <p className="text-[#130d20] text-center py-4">
                         {listdata.description}
                     </p>
                 </div>
-                <div className='w-full flex flex-col lg:flex-row container mx-auto gap-12 overflow-y-auto'>
-                    {
-                        listdata.informations.map((information, index) => {
-                            return (
-                                <div key={index} className='bg-[#204D67] w-full lg:w-1/3 p-8 rounded-[8px]'>
-                                    <h3 className='text-white text-2xl pb-4 text-center border-b-2 border-white'>{information.title}</h3>
-                                    <p className='text-white text-sm text-justify py-4 whitespace-pre-wrap'>{information.description}</p>
-                                </div>
-                            )
-                        })
-                    }
 
+                <div className='relative w-full overflow-hidden'>
+                    <div className='bg-gradient-to-l from-white to-transparent w-[2vw] h-full absolute right-0' />
+                    <div className='bg-gradient-to-r from-white to-transparent w-[2vw] h-full absolute left-0' />
+                    <div className='flex gap-8 overflow-x-auto w-full'>
+                        {
+                            listdata.informations.map((information, index) => {
+                                return (
+                                    <div key={index} className='bg-gray-300 min-w-full md:min-w-[20vw] rounded-[8px] overflow-hidden my-4 shadow-lg border-2 border-black/10'>
+                                        <Image src={information.imagesrc} alt='flyer' width={720} height={300} />
+                                    </div>
+                                )
+                            })
+                        }
+
+                    </div>
                 </div>
             </div>
         </SectionComp>
